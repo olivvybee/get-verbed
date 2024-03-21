0ac44826951e066cb40db9f0b2ecf94c7fa7f5cd
Liv Asch <olivia.c.asch@gmail.com>
2024-03-21 21:18:53 +0000

    Implement post generation

---
+++ arrays.ts
@@ -0,0 +1,24 @@
+export const VERBS = [
+  'posted',
+  'beaned',
+  'licked',
+  'shonked',
+  'rekt',
+  'vibrated',
+  "awawawa'd",
+  'snoofed',
+  'networked',
+  "maeow'd",
+  'boosted',
+  'migrated',
+  'patted',
+  'woofed',
+  'woemed',
+  'swapped',
+  'cached',
+  'foxed',
+  'typed',
+  '[contributed](https://github.com/olivvybee/get-verbed)',
+];
+
+export const SUBJECTS = ['nerd', 'dork', 'cutie', 'bro', 'm8', 'pal'];
--- generate.ts
+++ generate.ts
@@ -1,1 +1,13 @@
-export const generate = () => 'This is a test';
+import { VERBS, SUBJECTS } from './arrays';
+
+export const generate = () => {
+  const verb = randomElement(VERBS);
+  const subject = randomElement(SUBJECTS);
+
+  return `Get ${verb}, ${subject}`;
+};
+
+const randomElement = <T>(array: T[]): T => {
+  const index = Math.floor(Math.random() * array.length);
+  return array[index];
+};
