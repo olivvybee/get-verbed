5c66441a420caaec1442d7acf22b3456080c298a
Liv Asch <olivia.c.asch@gmail.com>
2024-03-21 21:28:05 +0000

    Add duplicate prevention

--- generate.ts
+++ generate.ts
@@ -1,10 +1,56 @@
+import path from 'path';
+import fs from 'fs';
+
 import { VERBS, SUBJECTS } from './arrays';
 
+const USED_VERBS_PATH = path.resolve('.', 'usedVerbs');
+const USED_SUBJECTS_PATH = path.resolve('.', 'usedSubjects');
+
 export const generate = () => {
-  const verb = randomElement(VERBS);
-  const subject = randomElement(SUBJECTS);
+  const usedVerbs = getUsedWords(USED_VERBS_PATH);
+  const usedSubjects = getUsedWords(USED_SUBJECTS_PATH);
+
+  let unusedVerbs = VERBS.filter((verb) => !usedVerbs.includes(verb));
+  let unusedSubjects = SUBJECTS.filter(
+    (subject) => !usedSubjects.includes(subject)
+  );
+
+  if (!unusedVerbs.length) {
+    resetUsedWords(USED_VERBS_PATH);
+    unusedVerbs = VERBS;
+  }
+
+  if (!unusedSubjects.length) {
+    resetUsedWords(USED_SUBJECTS_PATH);
+    unusedSubjects = SUBJECTS;
+  }
+
+  const verb = randomElement(unusedVerbs);
+  const subject = randomElement(unusedSubjects);
+
+  usedVerbs.push(verb);
+  usedSubjects.push(subject);
+
+  saveUsedWord(USED_VERBS_PATH, verb);
+  saveUsedWord(USED_SUBJECTS_PATH, subject);
 
   return `Get ${verb}, ${subject}`;
+};
+
+const getUsedWords = (path: string) => {
+  const contents = fs.readFileSync(path, 'utf-8');
+  return contents.split('\n').filter((str) => !!str.length);
+};
+
+const saveUsedWord = (path: string, word: string) => {
+  const usedWords = getUsedWords(path);
+  usedWords.push(word);
+  const contents = usedWords.join('\n');
+  fs.writeFileSync(path, contents);
+};
+
+const resetUsedWords = (path: string) => {
+  fs.writeFileSync(path, '');
 };
 
 const randomElement = <T>(array: T[]): T => {
