8b4f4f453843316d080f357def557b78ca96ad4e
Liv Asch <olivia.c.asch@gmail.com>
2024-03-24 09:37:21 +0000

    Arrays can have some gerrymandering as a treat (#16)

--- arrays.ts
+++ arrays.ts
@@ -86,6 +86,13 @@ export const VERBS: Verb[] = [
   'bit',
   '1-gritted',
   'perceived',
+  'gerrymandered',
+  "lego'd",
+  'melted',
+  'exploded',
+  'texted',
+  'dated',
+  'snuffled',
 ];
 
 export const SUBJECTS: string[] = [
