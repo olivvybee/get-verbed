10fa724e36738188238cef96f9f3de53ed203839
Liv Asch <olivia.c.asch@gmail.com>
2024-04-06 23:06:31 +0100

    Trailing comma

--- arrays.ts
+++ arrays.ts
@@ -152,7 +152,7 @@ export const VERBS: Verb[] = [
   'dereferenced',
   'downloaded',
   'uploaded',
-  { word: 'boofed', cw: 'lewd?' }
+  { word: 'boofed', cw: 'lewd?' },
 ];
 
 export const SUBJECTS: string[] = [
