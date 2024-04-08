68eccfbc67463658cdbae7828f58fbaab5acc777
Liv Asch <olivia.c.asch@gmail.com>
2024-04-08 11:15:31 +0100

    Get tinned and ionised (#37)

--- arrays.ts
+++ arrays.ts
@@ -115,6 +115,7 @@ export const VERBS: Verb[] = [
   'administrated',
   'unionised',
   'deionised',
+  'ionised',
   'held',
   'comforted',
   'empowered',
@@ -155,6 +156,7 @@ export const VERBS: Verb[] = [
   { word: 'boofed', cw: 'lewd?' },
   'minted',
   'calibrated',
+  'tinned',
 ];
 
 export const SUBJECTS: string[] = [
